c837aa334d2948fc25b56773ef1b0a7b73938b04
Clawz-123 <pukarbohara9@gmail.com>
2025-07-22 16:02:58 +0545

    installing swagger

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,27 @@
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Blog API",
+        default_version='v1',
+        description="API for blog posts and categories.",
+        terms_of_service="https://yourdomain.com/terms/",
+        contact=openapi.Contact(email="contact@yourdomain.com"),
+        license=openapi.License(name="MIT License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('blog.urls', namespace='blog')),
-    path('api/', include('blog_api.urls', namespace='blog_api')),
+    path('', include(('blog.urls', 'blog'), namespace='blog')),
+    path('api/', include(('blog_api.urls', 'blog_api'), namespace='blog_api')),
+
+    # Swagger Docs
+    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
